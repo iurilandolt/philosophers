e8bb8541f29610e9d6d41fec96413c7aa959fa74
iuri~ <123178126+iurilandolt@users.noreply.github.com>
2024-01-11 14:01:40 +0000

    tests with pthreads_t

--- threadss.c
+++ threadss.c
@@ -4,3 +4,24 @@
 #include <pthread.h>
 
 // cc -lpthread main.c
+
+void *routine(void)
+{
+ printf("Test Start:\n");
+ sleep(3);
+ printf("End Test.\n");
+}
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	pthread_t t1, t2;
+
+	pthread_create(&t1, NULL, routine, NULL);
+	pthread_create(&t2, NULL, routine, NULL);
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+}
